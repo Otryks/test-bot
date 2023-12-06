268ef3f1cf00072c2515e6451d224814104b5d25
otryks <otryksgames@gmail.com>
2023-12-06 21:35:12 +0100

    asd

---
+++ commands/utility/help.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('help')
+		.setDescription('List of commands.'),
+	async execute(interaction) {
+		// eslint-disable-next-line quotes
+		await interaction.reply(`**Ping** - replies with pong
+        **Server** - sends info about this server
+        **User** - sends info about you`);
+	},
+};
